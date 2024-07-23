f0a9179b1b674d9c6c747ac700f789101f5266ae
Ikfan13 <mohamedikfan1312@gmail.com>
2024-07-23 19:13:31 +0530

    Python_tasks

---
+++ Python Tasks/Task.py
@@ -0,0 +1,9 @@
+msg1="Hello"
+print("Hello in English: ",msg1)
+msg2="Bonjour"
+print("Hello in French: ",msg2)
+
+msg1="konichiwa"
+print("Hello in Japnese: ",msg1)
+msg2="Hola"
+print("Hello in Spanish: ",msg2)
